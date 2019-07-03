e4e242352c9664458885697dfb5019a069a4ced5
Matthew Lui <mlui@uniting.org>
2019-07-03 14:34:05 +1000

    updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 // Arrays
 const fruits = ['banana', 'apples', 'grapes'];
 console.log(fruits);
+
+const name = 'Matt';
+const age = 40;
+const job = 'Web developer';
+const city = 'Sydney';
+let html;
+
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+  </ul>
+`;
+
+let body = document.querySelector('body');
+body.innerHTML = '<h1>Hello</h1>';
+
+// document.body.innerHTML = html;
